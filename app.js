1d8e8bd978159b3d38aa35afc9e904dcdd7d9441
Youlong Li <chongzizil@gmail.com>
2015-03-04 01:02:09 -0500

    Add Triangle and cube.

--- app.js
+++ app.js
@@ -5,6 +5,9 @@
 angular.module('computerGraphics', ['ngRoute']).config(['$routeProvider',
   function($routeProvider) {
     $routeProvider.
+        when('/Contents', {
+            templateUrl: 'Contents.html'
+        }).
         when('/hw1', {
           templateUrl: 'homework/hw1/hw1.html'
         }).
@@ -29,7 +32,22 @@ angular.module('computerGraphics', ['ngRoute']).config(['$routeProvider',
         when('/Bounce', {
             templateUrl: 'homework/hw3/Bounce.html'
         }).
+        when('/Experiment', {
+            templateUrl: 'homework/hw4/Experiment.html'
+        }).
+        when('/Refraction', {
+            templateUrl: 'homework/hw4/Refraction.html'
+        }).
+        when('/Cylinder', {
+            templateUrl: 'homework/hw4/Cylinder.html'
+        }).
+        when('/Cube', {
+          templateUrl: 'homework/hw4/Cube.html'
+        }).
+        when('/Triangle', {
+          templateUrl: 'homework/hw4/Triangle.html'
+        }).
         otherwise({
-          redirectTo: '/'
+          redirectTo: '/Index'
         });
   }]);
